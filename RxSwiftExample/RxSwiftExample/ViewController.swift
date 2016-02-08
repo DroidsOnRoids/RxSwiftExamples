8c2870d2e39a6c2ebe27641336df0ff8201a8aad
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2016-02-08 14:12:01 +0100

    RxSwiftExample init

---
+++ RxSwiftExample/RxSwiftExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwiftExample
+//
+//  Created by Lukasz Mroz on 08.02.2016.
+//  Copyright © 2016 Droids on Roids. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
