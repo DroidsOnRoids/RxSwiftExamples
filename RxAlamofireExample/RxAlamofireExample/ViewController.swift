af5fb4403dfd860dc68bdc672dda240b06063046
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2016-02-10 15:28:01 +0100

    Comment to RxAlamofireExample

--- RxAlamofireExample/RxAlamofireExample/ViewController.swift
+++ RxAlamofireExample/RxAlamofireExample/ViewController.swift
@@ -5,6 +5,25 @@
 //  Created by Lukasz Mroz on 10.02.2016.
 //  Copyright © 2016 Droids on Roids. All rights reserved.
 //
+//
+//  In this app we connect few libraries to make repository search
+//  through GitHub API. Lets split it for few steps.
+//  First step would be to setup RxSwift so we can observe text
+//  from UISearchBar and get the updates. When we get the update,
+//  we have to make sure we are not spamming API. So we will make 
+//  use of throttle(_:scheduler) and distinctUntilChanged(). Also
+//  remember to use it in the same order as in the example (to make
+//  sure you get updates from the main thread always, because
+//  otherwise distinct might not work correctly). Then the next step
+//  would be to get the actual query from UISearchBar and now, that
+//  we know that we will not spam API, we can make a Alamofire 
+//  request. To do it, we will also use RxSwift, now with the 
+//  RxAlamofire wrapper. It makes using Alamofire nice and clean,
+//  as you can see in the code. We will make a requst to GitHub
+//  API and fetch the request for given username. To parse json
+//  array into Repository objects we will use ObjectMapper here.
+//  And thats it! Not that hard, right?
+//
 
 import UIKit
 import ObjectMapper
