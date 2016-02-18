f81330e5e5b0b9c2b611d3331b2f87acd766b55f
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2016-02-18 18:57:08 +0100

    RxMKMapView update.

--- RxMKMapViewExample/RxMKMapViewExample/ViewController.swift
+++ RxMKMapViewExample/RxMKMapViewExample/ViewController.swift
@@ -6,13 +6,49 @@
 //  Copyright © 2016 Droids on Roids. All rights reserved.
 //
 
+import MapKit
+import RxSwift
+import RxCocoa
+import RxMKMapView
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupRx()
     }
-
+    
+    func setupRx() {
+        mapView
+            .rx_willStartLoadingMap
+            .subscribeNext {
+                print("Will start loading map")
+            }
+            .addDisposableTo(disposeBag)
+        mapView
+            .rx_didFinishLoadingMap
+            .subscribeNext {
+                print("Finished loading map")
+            }
+            .addDisposableTo(disposeBag)
+        mapView
+            .rx_willStartRenderingMap
+            .subscribeNext {
+                print("Will start rendering map")
+            }
+            .addDisposableTo(disposeBag)
+        mapView
+            .rx_didFinishRenderingMap
+            .subscribeNext { fullyRendered in
+                print("Finished rendering map? Is it fully rendered tho? Of course \(fullyRendered)!")
+            }
+            .addDisposableTo(disposeBag)
+    }
+    
 }
 
