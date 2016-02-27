ce2afdaeda39daac3c8e2fef2411ffc29bb409a1
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2016-02-28 00:28:12 +0100

    Initial calculator project

---
+++ Simple Apps/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Lukasz Mroz on 26.02.2016.
+//  Copyright © 2016 Droids on Roids. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
